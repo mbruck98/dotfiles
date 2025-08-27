e1d86887ff89794c65dca8dd1b84e8838f381e94
mbruck98 <matej.bruck@gmail.com>
2025-08-28 01:20:34 +0200

    Add wlogout

---
+++ wlogout/shutdownmenu.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+if pgrep -x "wlogout" >/dev/null; then
+    pkill -x "wlogout"
+    exit 0
+fi
+
+[ -n "${1}" ] && wlogoutStyle="${1}"
+wlogoutStyle=${wlogoutStyle:-$WLOGOUT_STYLE}
+srcDir="$HOME/.config/wlogout"
+wLayout="${srcDir}/layout_${wlogoutStyle}"
+wlTmplt="${srcDir}/style_${wlogoutStyle}.css"
+
+x_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .width')
+y_mon=$(hyprctl -j monitors | jq '.[] | select(.focused==true) | .height')
+hypr_scale=$(hyprctl -j monitors | jq '.[] | select (.focused == true) | .scale' | sed 's/\.//')
+
+case "${wlogoutStyle}" in
+1)
+    wlColms=5
+    export mgn=$((y_mon * 28 / hypr_scale))
+    export hvr=$((y_mon * 23 / hypr_scale))
+    ;;
+2)
+    wlColms=2
+    export x_mgn=$((x_mon * 35 / hypr_scale))
+    export y_mgn=$((y_mon * 25 / hypr_scale))
+    export x_hvr=$((x_mon * 32 / hypr_scale))
+    export y_hvr=$((y_mon * 20 / hypr_scale))
+    ;;
+esac
+
+export fntSize=$((y_mon * 2 / 100))
+
+wlStyle="$(envsubst <"${wlTmplt}")"
+
+wlogout -b "${wlColms}" -c 0 -r 0 -m 0 --layout "${wLayout}" --css <(echo "${wlStyle}") --protocol layer-shell
